0a8ca510cf6eb0cac2f8fce290dd553a3982b89c
Jake Arnett <jarnett34.js@gmail.com>
2023-10-04 10:38:41 -0400

    completed ShoppingList app

---
+++ workbook-1/workbook-1-exercise/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        System.out.println("Shopping List: Bread; Water; Soda; Lunch Meat; London Broil; " +
+                "Potatoes; Onions; Gravy; Stew Mix; Mustard");
+    }
+}
