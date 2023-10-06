033287cc125c5c8fb2442bc84c483e8c94187c95
Jake Arnett <jarnett34.js@gmail.com>
2023-10-05 21:03:43 -0400

    added comments for clarity to MortgageCalculator

--- financial-calculators/MortgageCalculator/src/main/java/com/pluralsight/MortgageCalculator.java
+++ financial-calculators/MortgageCalculator/src/main/java/com/pluralsight/MortgageCalculator.java
@@ -1,18 +1,19 @@
 package com.pluralsight;
-
+//imports
 import java.util.*;
 import java.text.*;
 
 public class MortgageCalculator {
-
+    //initialize input and format
     public static Scanner scan = new Scanner(System.in);
     public static DecimalFormat df = new DecimalFormat("0.00");
 
     public static void main(String[] args){
-
+        //declare variables
         double principal, interest_rate, monthly_payment, total_interest, interest_rate_decimal;
         int loan_length;
 
+        //assign variables from input
         System.out.print("Enter the principal: $");
         principal = getPrincipal();
         System.out.print("Enter the interest rate: ");
@@ -21,15 +22,17 @@ public class MortgageCalculator {
         System.out.print("Enter the length of the loan in years: ");
         loan_length = getYears();
 
+        //assign variables from calculations
         monthly_payment = getPayment(principal, interest_rate_decimal, loan_length);
         total_interest = getInterestTotal(monthly_payment, principal, loan_length);
 
-
+        //output
         System.out.println("A $" + principal + " loan at " + interest_rate + "% interest for " +
                 loan_length + " years would have a $" + df.format(monthly_payment) + " / month payment with " +
                 "a total interest of $" + df.format(total_interest));
     }
 
+    //methods
     public static double getPrincipal(){
         return scan.nextDouble();
     }
@@ -43,7 +46,7 @@ public class MortgageCalculator {
     }
 
     public static double getPayment(double principal, double interest_rate_decimal, int loan_length){;
-
+        //monthly payment math done here to avoid clutter in main method
         double payment = 0.0;
         interest_rate_decimal /= 12;
 
@@ -55,7 +58,7 @@ public class MortgageCalculator {
     }
 
     public static double getInterestTotal(double monthly_payment, double principal, int loan_length){
-
+        //interest total math done here to avoid clutter in main method
         double total_interest = 0.0;
         total_interest = (monthly_payment * (loan_length * 12)) - principal;
 
