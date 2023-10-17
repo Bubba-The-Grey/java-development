f0e8ee410e2a59e88e800317ec9422645f7c2476
Jake Arnett <jarnett34.js@gmail.com>
2023-10-17 09:19:12 -0400

    Updtated PayrollCalculator to save data to a file

--- workbook-3/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ workbook-3/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,14 +1,16 @@
 package com.pluralsight;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.text.*;
 public class PayrollCalculator {
     public static DecimalFormat df = new DecimalFormat("0.00");
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("employees.csv"));
+        System.out.print("Enter the name of the employee file to process: ");
+        BufferedReader reader = new BufferedReader(new FileReader(scan.nextLine()));
+        System.out.print("Enter the name of the payroll file to create (please include the extension of .csv or .json): ");
+        String option = scan.nextLine();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(option));
         String words = "";
         Employee[] employees = new Employee[8];
         int count = 0;
@@ -20,8 +22,24 @@ public class PayrollCalculator {
             }
         }
         reader.close();
-        for(int i = 0; i < employees.length; i++){
-            System.out.println("Employee ID: " + employees[i].getId() + " | Employee Name: " + employees[i].getName() + " | Employee Gross Pay: $" + df.format(employees[i].getGrossPay()));
+        if(option.contains("json")) {
+            writer.write("[\n");
+            for (int i = 0; i < employees.length; i++) {
+                writer.write("\t{ \"id\" : " + employees[i].getId() + ",  \"name\" : \"" + employees[i].getName() + "\", \"grossPay\" : " + df.format(employees[i].getGrossPay()) + " }");
+                if(i == employees.length - 1){
+                    writer.write("\n");
+                }
+                else{
+                    writer.write(",\n");
+                }
+            }
+            writer.write("]");
         }
+        else{
+            for (int i = 0; i < employees.length; i++) {
+                writer.write(employees[i].getId() + "|" + employees[i].getName() + "|" + df.format(employees[i].getGrossPay()) + "\n");
+            }
+        }
+        writer.close();
     }
 }
