efec1b562b04a05d9d9f2520fb14e8b8c822693b
Jake Arnett <jarnett34.js@gmail.com>
2023-12-11 13:41:00 -0500

    Working on Database Queries with Java

---
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root",
+                "D3athL33terN00b");
+        Statement statement = connection.createStatement();
+        String query = "SELECT * FROM products";
+        ResultSet results = statement.executeQuery(query);
+        while(results.next()){
+            System.out.println(results.getString(2));
+        }
+    }
+}
