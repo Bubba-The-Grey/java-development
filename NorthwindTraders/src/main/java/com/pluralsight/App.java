51bf092ca3fb29c119f163aa61375a8e40928179
Jake Arnett <jarnett34.js@gmail.com>
2023-12-12 09:14:53 -0500

    Yep

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -2,16 +2,22 @@ package com.pluralsight;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.*;
+import java.text.DecimalFormat;
 
 public class App {
+    public static DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root",
-                "D3athL33terN00b");
-        Statement statement = connection.createStatement();
-        String query = "SELECT * FROM products";
-        ResultSet results = statement.executeQuery(query);
+                "");
+        PreparedStatement statement = connection.prepareStatement("select * from products;");
+        ResultSet results = statement.executeQuery();
         while(results.next()){
-            System.out.println(results.getString(2));
+            System.out.println("ID: " + results.getString(1));
+            System.out.println("Name: " + results.getString(2));
+            System.out.println("Price: " + df.format(results.getFloat(6)));
+            System.out.println("Stock: " + results.getString(7));
+            System.out.println("__________________________________________________________");
+            System.out.println();
         }
         connection.close();
     }
