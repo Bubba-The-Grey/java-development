e1c20d80cdba99c45893326db56b7670f0df9161
Jake Arnett <jarnett34.js@gmail.com>
2023-12-13 11:47:25 -0500

    Added Sakila Movies Java app with SQL queries

---
+++ SakilaMovies/src/main/java/com/pluralsight/Movie.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class Movie {
+    private int filmID, releaseYear, length;
+    private String title, description;
+
+    public Movie(int filmID, int releaseYear, int length, String title, String description) {
+        this.filmID = filmID;
+        this.releaseYear = releaseYear;
+        this.length = length;
+        this.title = title;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("Film ID: " + filmID);
+        str.append("\nTitle: " + title);
+        str.append("\nReleased: " + releaseYear);
+        str.append("\nRun Time: " + length);
+        str.append("\nDescription: \n" + description);
+        str.append("\n_________________________________________________________________________________");
+        return str.toString();
+    }
+}
